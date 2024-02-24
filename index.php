8f3a324633ce88217b715ea6953fb6b3c595cba7
mira-kaas <mira-kaas@192.168.10.104>
2024-02-24 13:02:45 +0100

    Test číslo 1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     </head>
     <body>
         <?php
-        echo 'Hello World!';
+        echo 'Hello World 2!';
         ?>
     </body>
 </html>
